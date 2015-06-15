4926e017ab7427f0686bda0b54211eef4007a0fb
Alex Satterly & John Lyden <pair+alcsatt.johnlyden@devbootcamp.com>
2015-06-15 10:43:26 -0400

    adding cars_controller, index view for cars, migration. everything works so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/cars' => 'cars#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
